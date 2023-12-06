c249fc2ee7539c1de8fe84cdacee723574be7fa2
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2023-12-07 00:53:13 +0600

    :rocket:

---
+++ mupl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mupl
+//
+//  Created by Tamerlan Satualdypov on 07.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mupl/muplApp.swift
@@ -0,0 +1,17 @@
+//
+//  muplApp.swift
+//  mupl
+//
+//  Created by Tamerlan Satualdypov on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct muplApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
