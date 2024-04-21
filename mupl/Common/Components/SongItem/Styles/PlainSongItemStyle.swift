db2341c8198baa71d67b8f4fd17e1cb5e4e6a5ea
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2024-04-21 21:03:19 +0500

    Added native SF symbols animations

--- mupl/Common/Components/SongItem/Styles/PlainSongItemStyle.swift
+++ mupl/Common/Components/SongItem/Styles/PlainSongItemStyle.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import MusicKit
-import RiveRuntime
 
 struct PlainSongItemStyle: SongItemStyle {
     func content(for song: Song, context: SongItemContext) -> some View {
@@ -32,9 +31,14 @@ struct PlainSongItemStyle: SongItemStyle {
                                     Color.black
                                         .opacity(0.4)
                                     
-                                    RiveViewModel(fileName: "soundwave", stateMachineName: "State")
-                                        .view()
-                                        .frame(width: 16.0, height: 16.0)
+                                    Image(systemName: "waveform")
+                                        .font(.system(size: 14.0))
+                                        .foregroundStyle(Color.white)
+                                        .symbolEffect(
+                                            .variableColor.iterative.dimInactiveLayers,
+                                            options: .repeating,
+                                            isActive: context.isCurrentlyPlaying
+                                        )
                                 }
                             }
                         }
